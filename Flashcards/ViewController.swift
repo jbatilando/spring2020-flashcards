68e067c4707d444892f0ba3b12ddb396abc07185
Miguel Batilando <miguel.batilando@gmail.com>
2020-02-06 19:29:52 -0800

    Finished all optional features

--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -12,15 +12,44 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var frontLabel: UILabel!
     @IBOutlet weak var backLabel: UILabel!
+    @IBOutlet weak var card: UIView!
+    @IBOutlet weak var firstButton: UIButton!
+    @IBOutlet weak var secondButton: UIButton!
+    @IBOutlet weak var thirdButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Card container
+        self.card.layer.cornerRadius = 20.0
+        self.card.layer.shadowRadius = 15.0
+        self.card.layer.shadowOpacity = 0.3
+        // Question and answer
+        self.frontLabel.clipsToBounds = true
+        self.frontLabel.layer.cornerRadius = 20.0
+        self.backLabel.clipsToBounds = true
+        self.backLabel.layer.cornerRadius = 20.0
+        // Buttons
+        self.firstButton.layer.borderWidth = 3.0
+        self.firstButton.layer.borderColor = #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)
+        self.firstButton.layer.cornerRadius = 20.0
+        self.secondButton.layer.borderWidth = 3.0
+        self.secondButton.layer.borderColor = #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)
+        self.secondButton.layer.cornerRadius = 20.0
+        self.thirdButton.layer.borderWidth = 3.0
+        self.thirdButton.layer.borderColor = #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)
+        self.thirdButton.layer.cornerRadius = 20.0
     }
-
-    @IBAction func didTapOnFlashcards(_ sender: Any) {
+    
+    @IBAction func didTapFirstButton(_ sender: Any) {
+        self.firstButton.isHidden = true
+    }
+    
+    @IBAction func didTapSecondButton(_ sender: Any) {
         self.frontLabel.isHidden = true
     }
     
+    @IBAction func didTapThirdButton(_ sender: Any) {
+        self.thirdButton.isHidden = true
+    }
 }
 
