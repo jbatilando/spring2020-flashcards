fb9f69e7875efb14008b3420224dcc9e6010fefd
Miguel Batilando <miguel.batilando@gmail.com>
2020-02-06 17:51:28 -0800

    First commit

---
+++ Flashcards/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flashcards
+//
+//  Created by Miguel Batilando on 2/6/20.
+//  Copyright © 2020 miguel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
